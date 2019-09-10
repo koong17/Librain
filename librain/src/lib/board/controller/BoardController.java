e53fcc9e49546b7f51b7327307d269f99b5f3b05
seriously <stint@daum.net>
2019-09-10 16:02:33 +0900

    start

--- librain/src/lib/board/controller/BoardController.java
+++ librain/src/lib/board/controller/BoardController.java
@@ -1,7 +1,6 @@
 package lib.board.controller;
 
 public class BoardController {
-	kkkkkkkkk
 	//Controller�� ���ⲯ �����ʽÿ�
 	//(��ü�˻�)selectAll //(�˻�)select //(����)update //(����)insert //(����)delete
 	//�����̸��� ù���� �ҹ��� camelCase
