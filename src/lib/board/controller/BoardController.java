6db246a0315fb3f2161cf57ab8ea31daa058f001
passion1111 <se0572@naver.com>
2019-09-11 18:24:17 +0900

    스프링 config설정 고칠것

---
+++ src/lib/board/controller/BoardController.java
@@ -0,0 +1,11 @@
+package lib.board.controller;
+
+public class BoardController {
+
+	//Controller는 취향껏 나누십시오
+	//(전체검색)selectAll //(검색)select //(수정)update //(삽입)insert //(삭제)delete
+	//변수이름은 첫글자 소문자 camelCase
+	//메소드는 기능 설명이 가능하게 명명
+	//변수명__VO는 DB칼럼명이랑 똑같이
+	
+}
