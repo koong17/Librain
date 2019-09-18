5afe87efa0fff6aec58089af39b83e5945f1f52b
HyesooMoon <it_hyesoo@naver.com>
2019-09-18 15:18:44 +0900

    15:18

--- src/main/webapp/resources/js/book.js
+++ src/main/webapp/resources/js/book.js
@@ -1,11 +1,17 @@
 
 function searchAjax() {
-	
-	var searchCtgr = $('#searchCtgr').val();
-	var searchWord = $('#searchWord').val();
+	var sendData = {
+		searchCtgr: $('#searchCtgr').val(),
+		searchWord: $('#searchWord').val()
+	}
 	
 	$.ajax({
 		type: "Post",
-		url: "search.do"	
+		url: "search.do",
+		data: JSON.strigify(sendData),
+		dataType: "json",
+		contentType: "applicaition/json; charset=UTF-8",
+		async: true,
+		
 	})
 }
